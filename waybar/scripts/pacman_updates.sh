29ceb427b8bb94e8011b4810205aab3a59fafc43
codeblake <codeblake@outlook.com>
2023-03-08 11:53:18 +0000

    Added package versions to waybar tooltip

--- waybar/scripts/pacman_updates.sh
+++ waybar/scripts/pacman_updates.sh
@@ -8,5 +8,6 @@ if [ $? -eq 1 ]; then
 fi
 count=$(echo "${output}" | wc -l)
 pkgnames=$(echo "${output}" | awk '{print$1}')
+versions=$(echo "${output}" |  sed -z 's/\n/\\n/g')
 [ "${count}" -gt 0 ] && notify-send "Pacman" "${count} package updates available"
-echo "{\"text\":\""${count}"\",\"alt\":\""${pkgnames}"\",\"tooltip\":\""${pkgnames}"\"}"
+echo "{\"text\":\""${count}"\",\"alt\":\""${pkgnames}"\",\"tooltip\":\""${versions::-2}"\"}"
