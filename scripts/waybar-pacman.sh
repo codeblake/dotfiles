444e76f5175abebc32398e2456805cacf271831d
codeblake <codeblake@outlook.com>
2023-03-21 19:09:45 +0000

    Fixed waybar-pacman.sh when up to date

--- scripts/waybar-pacman.sh
+++ scripts/waybar-pacman.sh
@@ -2,13 +2,18 @@
 
 echo "{\"text\": \"checking updates…\"}"
 output=$(sudo checkupdates)
+
 if [ $? -eq 1 ]; then
     echo "{\"text\": \"ERROR: Cannot fetch updates\"}"
     exit 1
+elif [ -z $output ]; then
+    echo "{\"text\":0,\"alt\":\"Up to date\",\"tooltip\":\"No packages to update\"}"
+    exit 0
 fi
-count=$(echo "$(echo "${output}" | wc -l) - 1" | bc )
+
+count=$(echo "${output}" | wc -l)
 pkgnames=$(echo "${output}" | awk '{print$1}')
 versions=$(echo "${output}" | sed -z 's/\n/\\n/g; s/->/→/g')
 
 [ "${count}" -gt 0 ] && notify-send "Pacman" "${count} package updates available"
-echo "{\"text\":\""${count}"\",\"alt\":\""${pkgnames}"\",\"tooltip\":\""${versions::-2}"\"}"
+echo "{\"text\":"${count}",\"alt\":\""${pkgnames}"\",\"tooltip\":\""${versions::-2}"\"}"
