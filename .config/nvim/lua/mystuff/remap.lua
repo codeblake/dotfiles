f5228c6435474c5e4aa150127f0c97aedcda62d2
codeblake <codeblake@outlook.com>
2023-06-16 16:22:43 +0100

    Added lua nvim config

---
+++ .config/nvim/lua/mystuff/remap.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n","j","<up>")
+vim.keymap.set("n","k","<down>")
+vim.keymap.set("n","l","<left>")
+vim.keymap.set("n",";","<right>")
---
+++ .config/nvim/lua/mystuff/set.lua
@@ -0,0 +1,21 @@
+vim.opt.relativenumber=true
+vim.opt.smartindent=true
+vim.opt.incsearch=true
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.opt.expandtab=true
+vim.opt.listchars.eol="↴"
+vim.opt.listchars.tab="➔ "
+vim.opt.listchars.trail="~"
+vim.opt.listchars.extends="»"
+vim.opt.listchars.precedes="«"
+vim.opt.listchars.space="·"
+vim.opt.clipboard={unnamed,unnamedplus}
+vim.opt.spelllang="en_gb"
+vim.opt.showmode=true               
+vim.opt.autochdir=true
+vim.opt.hidden=true
+vim.opt.wrap=true
+vim.opt.list=true      
+
